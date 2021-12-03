58d0c13226906160882cb21b20ddb7144140d5c4
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2021-12-03 14:58:33 +0100

    file read from source

--- file2.php
+++ file2.php
@@ -10,10 +10,12 @@ if(!empty($_POST['path'])){
 $hn = opendir($_POST['path']);
 while($file = readdir($hn)){
 if(is_dir($_POST['path']."/".$file)){
-    echo "$file è una cartella</br>";
+ //  echo "$file è una cartella</br>";
+    echo "<a href=ardizzone.pietro.tave.osdb.it/".$_POST['path']."/".$file.">".$file."</a> è una cartella</br>";
 }else{
-    echo "$file è un file</br>";
-   
+    
+    //echo "$file è un file</br>";
+    echo "<a href=".$_POST['path']."/".$file.">".$file."</a> è un file</br>";
 }
 }
 closedir($hn);
